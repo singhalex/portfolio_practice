22f6b9270618a52e2a90b0a515cf96975dc6c576
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-11-09 10:22:49 -0800

    Create desktop layout for portfolio site

---
+++ src/components/projects.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    name: "Shopping Cart",
+    description:
+      "The goal of this project was to put together all of my React experience into an application that uses client side routing. Using React Router and the FakeStore API, this project has multiple pages common to a webstore.",
+    screenshot:
+      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    github: "https://github.com/singhalex/shopping-cart",
+    link: "https://serene-rolypoly-762f58.netlify.app/",
+  },
+  {
+    name: "Memory Card",
+    description:
+      "Users try and click on pokemon without repeating. Every time the user clicks on a card, the cards are shuffled. High scores are tracked over the session.",
+    screenshot:
+      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    github: "https://github.com/singhalex/memory-card",
+    link: "https://rainbow-moxie-5be9bb.netlify.app/",
+  },
+  {
+    name: "CV Creator",
+    description: "A React app that allows users to create a resume.",
+    screenshot:
+      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    github: "https://github.com/singhalex/cv-application",
+    link: "https://singhalex.github.io/cv-application/",
+  },
+  {
+    name: "Weather App",
+    description:
+      "This project was an assignment for The Odin Project. The goal was to learn how to use asynchronous function in combination with APIs.",
+    screenshot:
+      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    github: "https://github.com/singhalex/weather",
+    link: "https://singhalex.github.io/weather/",
+  },
+  {
+    name: "Library",
+    description:
+      "Using HTML, CSS, and Javascript, I built a page that displays and tracks the read status of books entered by the user.",
+    screenshot:
+      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    github: "https://github.com/singhalex/library-odin",
+    link: "https://www.theodinproject.com/lessons/node-path-javascript-library",
+  },
+  {
+    name: "Battleship",
+    description:
+      "A game of battleship against a computer opponent built using Javascript, HTML, and CSS.",
+    screenshot:
+      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    github: "https://github.com/singhalex/battleship",
+    link: "https://www.google.com",
+  },
+];
+
+export default projects;
