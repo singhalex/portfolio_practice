8fa6367a9f74d478fb2bf5e1c5555ca3da37168a
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-11-13 16:03:07 -0800

    Update the image links

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,7 +4,7 @@ const projects = [
     description:
       "The goal of this project was to put together all of my React experience into an application that uses client side routing. Using React Router and the FakeStore API, this project has multiple pages common to a webstore.",
     screenshot:
-      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+      "https://user-images.githubusercontent.com/115970252/279821989-c2b72431-e8aa-466a-98c7-6c19c53ce1ba.png",
     github: "https://github.com/singhalex/shopping-cart",
     link: "https://serene-rolypoly-762f58.netlify.app/",
   },
@@ -13,7 +13,7 @@ const projects = [
     description:
       "Users try and click on pokemon without repeating. Every time the user clicks on a card, the cards are shuffled. High scores are tracked over the session.",
     screenshot:
-      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+      "https://user-images.githubusercontent.com/115970252/267102505-2effe0f6-3d3f-4cb8-bb13-c3e18b010990.png",
     github: "https://github.com/singhalex/memory-card",
     link: "https://rainbow-moxie-5be9bb.netlify.app/",
   },
@@ -21,16 +21,16 @@ const projects = [
     name: "CV Creator",
     description: "A React app that allows users to create a resume.",
     screenshot:
-      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+      "https://user-images.githubusercontent.com/115970252/282609167-b8fbedd2-f944-431c-8ad9-5b064cab93ce.png",
     github: "https://github.com/singhalex/cv-application",
     link: "https://singhalex.github.io/cv-application/",
   },
   {
     name: "Weather App",
     description:
-      "This project was an assignment for The Odin Project. The goal was to learn how to use asynchronous function in combination with APIs.",
+      "This project was an assignment for The Odin Project. The goal was to learn how to use asynchronous functions in combination with APIs.",
     screenshot:
-      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+      "https://user-images.githubusercontent.com/115970252/217642277-6a90e848-9f5c-4fb2-bb8f-185ddf87d3e4.png",
     github: "https://github.com/singhalex/weather",
     link: "https://singhalex.github.io/weather/",
   },
@@ -39,7 +39,7 @@ const projects = [
     description:
       "Using HTML, CSS, and Javascript, I built a page that displays and tracks the read status of books entered by the user.",
     screenshot:
-      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+      "https://user-images.githubusercontent.com/115970252/209237583-18d9f80c-3e40-43f3-bd09-1e64f152ca11.png",
     github: "https://github.com/singhalex/library-odin",
     link: "https://www.theodinproject.com/lessons/node-path-javascript-library",
   },
@@ -48,7 +48,7 @@ const projects = [
     description:
       "A game of battleship against a computer opponent built using Javascript, HTML, and CSS.",
     screenshot:
-      "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+      "https://user-images.githubusercontent.com/115970252/282622813-69095d0a-1902-4da3-b78c-faa29fb74e07.png",
     github: "https://github.com/singhalex/battleship",
     link: "https://www.google.com",
   },
